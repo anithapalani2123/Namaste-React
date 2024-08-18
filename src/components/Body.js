8731efe82c1525ab565b03eef0018d6ab33b19ee
anithapalani2123 <anithapalani2004april@gmail.com>
2024-08-18 13:43:42 +0530

    usestate

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,21 @@
 import ResCard from "./ResCard";
 import dataApi from "../utils/mockData";
+import { useState } from "react";
 
 
 const Body = () => {
+
+    const [data,setData]=useState(dataApi);
     return(
         <div className='body'>
-            <div className='search'>Search</div>
+            <button className='filter-restaurants' onClick={()=>{
+                const filteredData=data.filter((restaurant)=> restaurant.info.avgRating>4.5);
+                setData(filteredData);
+
+            }}>Top Rated Restaurants</button>
             <div className='Res-container'>
                 {
-                    dataApi.map((restaurant)=> 
+                    data.map((restaurant)=> 
                         <ResCard key={restaurant.info.id} resData={restaurant}/>
                     )
                 }

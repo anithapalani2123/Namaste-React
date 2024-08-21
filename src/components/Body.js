2ebaeedbf459aeb96e7f990d31db142ff48ae422
anithapalani2123 <anithapalani2004april@gmail.com>
2024-08-21 22:17:14 +0530

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,31 @@
 import ResCard from "./ResCard";
-import dataApi from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer"
+import { useEffect, useState } from "react";
 
 
 const Body = () => {
 
-    const [data,setData]=useState(dataApi);
+    const [data,setData]=useState([]);
+    useEffect(()=>{
+        fetchData();
+
+    },[]);
+
+    const fetchData = async () => {
+
+        const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=13.0693568&lng=80.1948314");
+        const json=await data.json();
+
+        console.log(json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+        setData(json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+
+
+    }
+
+    if(data.length===0)
+    {
+        return <Shimmer/>
+    }
     return(
         <div className='body'>
             <button className='filter-restaurants' onClick={()=>{
---
+++ src/components/Shimmer.js
@@ -0,0 +1,24 @@
+
+const Shimmer = () => {
+    return (
+        <div className="Shimmer-container">
+            <div className="Shimmer-cards"></div>
+            <div className="Shimmer-cards"></div>
+            <div className="Shimmer-cards"></div>
+            <div className="Shimmer-cards"></div>
+            <div className="Shimmer-cards"></div>
+            <div className="Shimmer-cards"></div>
+            <div className="Shimmer-cards"></div>
+            <div className="Shimmer-cards"></div>
+            <div className="Shimmer-cards"></div>
+            <div className="Shimmer-cards"></div>
+            <div className="Shimmer-cards"></div>
+            <div className="Shimmer-cards"></div>
+
+        </div>
+    )
+
+
+}
+
+export default Shimmer;

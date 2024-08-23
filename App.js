744b0e45fe5062133b8d98a0b23e11f24a396427
anithapalani2123 <anithapalani2004april@gmail.com>
2024-08-23 18:55:05 +0530

    React-Router

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './src/components/Header'
 import Body from './src/components/Body'
+import Contact from './src/components/Contact'
+import About from './src/components/About'
+import Error from './src/components/Error'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+
 
 
 const AppLayout = () =>{
@@ -12,5 +17,23 @@ const AppLayout = () =>{
         </div>
     )
 }
+
+const appRoutes = createBrowserRouter([
+    {
+        path:"/",
+        element: <AppLayout/>,
+        errorElement:<Error/>
+
+    },
+    {
+        path: "/about",
+        element: <About/>
+    },
+    {
+        path: "/contact",
+        element: <Contact/>
+    }
+]);
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>)
+root.render(<RouterProvider router={appRoutes}/>)
